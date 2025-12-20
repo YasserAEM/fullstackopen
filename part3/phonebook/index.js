ea007da6eab5b98b44421cfcaea07a71e68461b5
YasserAEM <81475229+YasserAEM@users.noreply.github.com>
2025-12-20 01:48:37 +0100

    eslint

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,94 +8,95 @@ app.use(express.static('dist'))
 
 app.use(express.json())
 
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
-app.use(morgan('tiny', {skip: (req, res) => req.method === "POST"}))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {skip: (req, res) => req.method !== "POST"}))
+app.use(morgan('tiny', { skip: (req) => req.method === 'POST' }))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', { skip: (req) => req.method !== 'POST' }))
 
 
 app.get('/api/persons', (request, response) => {
-    Person.find().then(persons => {
-        response.json(persons)
-    })
+  Person.find().then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${Person.find.length} people</p>
+  response.send(`<p>Phonebook has info for ${Person.find.length} people</p>
                    <p>${Date()}</p>`)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findById(id)
-        .then(person => {
-            if (!person) {
-                response.status(404).end()
-            } else {
-                response.json(person)
-            }
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findById(id)
+    .then(person => {
+      if (!person) {
+        response.status(404).end()
+      } else {
+        response.json(person)
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
+  const id = request.params.id
 
-    Person.findByIdAndDelete(id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
     })
-    
-    person.save()
-        .then(savedPerson => {
-        res.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const {name, number} = req.body
+  const { name, number } = req.body
 
-    Person.findById(req.params.id)
-        .then(person => {
-            person.name = name
-            person.number = number
+  Person.findById(req.params.id)
+    .then(person => {
+      person.name = name
+      person.number = number
 
-            person.save().then(updatedPerson => {
-                res.json(updatedPerson)
-            })
-        })
+      person.save().then(updatedPerson => {
+        res.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unkown endpoint'})
+  res.status(404).send({ error: 'unkown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.log(error)
+  console.log(error)
 
-    if (error.name === 'CastError') {
-        res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name ==='ValidationError') {
-        res.status(400).json({error: error.message})
-    }
+  if (error.name === 'CastError') {
+    res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name ==='ValidationError') {
+    res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`)
+  console.log(`server listening on port ${PORT}`)
 })
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument - optionally add name and phone number as arguments')
-    process.exit(1)
+  console.log('give password as argument - optionally add name and phone number as arguments')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,34 +11,34 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.wupodj1.mongodb.net/ph
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(url, {family: 4})
+mongoose.connect(url, { family: 4 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length > 3) {
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const person = new Person({
-        name: name,
-        number: number
-    })
+  const person = new Person({
+    name: name,
+    number: number
+  })
 
-    person.save().then(response => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    console.log('phonebook:')
-    Person.find().then(response => {
-        response.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  console.log('phonebook:')
+  Person.find().then(response => {
+    response.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 }
